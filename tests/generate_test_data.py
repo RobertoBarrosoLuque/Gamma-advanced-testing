dc424d38d10001e3472ea7c5f432f5f832da9165
Roberto Barroso-Luque <BarrosoLuque.Roberto@bcg.com>
2023-05-09 10:36:53 -0700

    update generste test data

--- tests/generate_test_data.py
+++ tests/generate_test_data.py
@@ -1,5 +1,5 @@
-from src.train.data import load_raw_data
+from src.train.data import load_raw_data, DATA_DIR
 
 if __name__ == "__main__":
     data = load_raw_data()
-    data.sample(n=20).to_json('sample.json')
+    data.sample(n=20).to_json(DATA_DIR / 'sample.json')
