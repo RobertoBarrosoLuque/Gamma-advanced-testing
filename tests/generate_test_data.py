6960f9a516f4a774b108728c40340f86669432b8
Roberto Barroso-Luque <BarrosoLuque.Roberto@bcg.com>
2023-05-09 10:39:11 -0700

    Use records format

--- tests/generate_test_data.py
+++ tests/generate_test_data.py
@@ -2,4 +2,4 @@ from src.train.data import load_raw_data, DATA_DIR
 
 if __name__ == "__main__":
     data = load_raw_data()
-    data.sample(n=20).to_json(DATA_DIR / 'sample.json')
+    data.sample(n=20).to_json(DATA_DIR / 'sample.json', orient="records")
