bd145803629d9e0b492148b27b9e8bcdb02f81a6
Roberto Barroso-Luque <BarrosoLuque.Roberto@bcg.com>
2023-05-12 09:32:28 -0700

    Re factor train.py

--- src/train/train.py
+++ src/train/train.py
@@ -1,5 +1,5 @@
 from logging import getLogger
-from typing import Tuple
+from typing import Tuple, Dict
 
 import numpy as np
 import pandas as pd
@@ -19,15 +19,14 @@ logger = getLogger(__name__)
 SCORING = 'neg_log_loss'
 
 
-def train(estimator: SklearnEstimator,
-          data: DataFrame,
-          optimize: bool = False,
-          log: bool = True) -> Tuple[float, np.ndarray]:
-    logger.debug(f'Training {estimator.__class__.__name__} on dataset...')
-
+def _train_test_split(data: DataFrame) -> Tuple:
     X, y = split_labels(data)
     X_train, X_test, y_train, y_test = train_test_split(X, y)
+    return X_train, X_test, y_train, y_test
 
+
+def _get_best_estimator_and_cv_log_loss(optimize: bool, estimator: SklearnEstimator, X_train: DataFrame,
+                                        y_train: DataFrame) -> Tuple[SklearnEstimator, np.ndarray]:
     if optimize:
         best_estimator, cv_log_losses = hyperopt(
             estimator, X_train, y_train, param_grid=get_param_grid()
@@ -35,6 +34,20 @@ def train(estimator: SklearnEstimator,
     else:
         cv_log_losses = cross_validate(estimator, X_train, y_train)
         best_estimator = estimator
+    return best_estimator, cv_log_losses
+
+
+def train(estimator: SklearnEstimator,
+          data: DataFrame,
+          optimize: bool = False,
+          log: bool = True) -> Tuple[float, np.ndarray]:
+    logger.debug(f'Training {estimator.__class__.__name__} on dataset...')
+
+    X_train, X_test, y_train, y_test = _train_test_split(data)
+
+    best_estimator, cv_log_losses = _get_best_estimator_and_cv_log_loss(
+        optimize=optimize, estimator=estimator, X_train=X_train, y_train=y_train
+    )
 
     best_estimator.fit(X_train, y_train)
     test_log_loss = evaluate(best_estimator, X_test, y_test)
@@ -56,26 +69,35 @@ def cross_validate(estimator: SklearnEstimator,
                             scoring=SCORING, cv=gen_kfold())
 
 
+def _fit_grid_search_obj(estimator, param_grid, X, y):
+    gs = GridSearchCV(estimator, param_grid, scoring=SCORING, cv=gen_kfold())
+    gs.fit(X, y)
+    return gs
+
+
+def _calculate_best_cv_loss(cv_results, grid_search_obj):
+    regex_log_loss_cv_cols = '^split[0-9]+_test_score'
+    best_cv_losses = -cv_results.loc[grid_search_obj.best_index_] \
+        .filter(regex=regex_log_loss_cv_cols)
+    return best_cv_losses
+
+
 def hyperopt(estimator: SklearnEstimator,
              X: DataFrame,
              y: DataFrame,
              param_grid: dict) -> Tuple[SklearnEstimator, np.ndarray]:
+
     logger.info(
         'Performing hyperparameter optimization with GridSearchCV...')
 
-    gs = GridSearchCV(estimator, param_grid, scoring=SCORING, cv=gen_kfold())
-    gs.fit(X, y)
+    grid_search_obj = _fit_grid_search_obj(estimator, param_grid, X, y)
 
-    best_loss = -gs.best_score_
-    logger.info(f'Best loss: {best_loss:.4f}')
+    logger.info(f'Best loss: {-grid_search_obj.best_score_:.4f}')
 
-    cv_results = pd.DataFrame(gs.cv_results_).sort_values(by='rank_test_score')
+    cv_results = pd.DataFrame(grid_search_obj.cv_results_).sort_values(by='rank_test_score')
+    best_cv_losses = _calculate_best_cv_loss(cv_results, grid_search_obj)
 
-    regex_log_loss_cv_cols = '^split[0-9]+_test_score'
-    best_cv_losses = -cv_results.loc[gs.best_index_] \
-        .filter(regex=regex_log_loss_cv_cols)
-
-    return gs.best_estimator_, best_cv_losses
+    return grid_search_obj.best_estimator_, best_cv_losses
 
 
 def evaluate(estimator: SklearnEstimator, X: DataFrame, y: DataFrame) -> float:
